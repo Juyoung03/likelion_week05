8c76bbf49d11a352842b45fb5a75804e14baea13
Juyoung03 <iris031008@naver.com>
2025-07-22 11:50:42 +0900

    소셜 로그인 구현

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -1,25 +1,29 @@
-import kakaoImg from "../assets/kakao.svg";
+//import kakaoImg from "../assets/kakao.svg";
 
 const SocialLogin = () => {
-  return (
-    <main className="rounded-lg shadow-lg h-[640px] flex flex-col items-center justify-center dt:w-[448px] ph:w-[370px]">
-      <div className="flex flex-col text-center mb-[50px] text-[20px] font-bold">
-        <p>카카오톡으로 간편하게 로그인하고</p>
-        <p>서비스를 이용해보세요!</p>
-      </div>
+  const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+  const REST_API_KEY = import.meta.env.VITE_KAKAO_API_KEY;
 
-      <div>
-        <button className="relative rounded-[6px] bg-[#FEE500] dt:w-[400px] ph:w-[322px] h-[40px] text-white">
-          <p className="font-extrabold text-[15px] text-black">
-            카카오톡으로 로그인
-          </p>
-          <img
-            src={kakaoImg}
-            alt="kakao"
-            className="absolute bottom-[6px] right-[17px]"
-          />
-        </button>
-      </div>
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
+
+  const handleLogin = () => {
+    window.location.href = kakaoURL;
+  }
+  
+  return (
+    <main className="flex flex-col items-center justify-center h-full flex-1 bg-white">
+      <h1 className="text-[#191919] text-2xl font-semibold text-center mb-8">
+        카카오톡으로 간편하게 로그인하고 <br />
+        서비스를 이용해보세요!
+      </h1>
+
+      <button 
+        onClick={handleLogin} 
+        className="flex items-center justify-center gap-2 w-[280px] py-3 cursor-pointer rounded-md bg-[#FEE500] text-[#191919] hover:opacity-90 transition"
+      >
+        {/* <img src={kakaoLogo} alt="kakao" className="w-6 h-6" /> */}
+        <span className="text-base font-semibold">카카오로 계속하기</span>
+      </button>
     </main>
   );
 };
